54f26893f70bf948fc0c3fb75fc385c17201807c
Christian Kurzke <cgk@slipstreamsystems.com>
2022-03-03 03:45:16 -0800

    Setup for 12F1572

--- nbproject/Makefile-variables.mk
+++ nbproject/Makefile-variables.mk
@@ -4,6 +4,10 @@
 # NOCDDL
 #
 CND_BASEDIR=`pwd`
+# PIC_12F1572 configuration
+CND_ARTIFACT_DIR_PIC_12F1572=dist/PIC_12F1572/production
+CND_ARTIFACT_NAME_PIC_12F1572=pic12_c_sequencer.X.production.hex
+CND_ARTIFACT_PATH_PIC_12F1572=dist/PIC_12F1572/production/pic12_c_sequencer.X.production.hex
 # XC8_PIC12F609 configuration
 CND_ARTIFACT_DIR_XC8_PIC12F609=dist/XC8_PIC12F609/production
 CND_ARTIFACT_NAME_XC8_PIC12F609=pic12_c_sequencer.X.production.hex
